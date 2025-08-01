e3f47d266fff9170a4ded14ad732eb6e39bb6a98
Michiel <michielp1807@gmail.com>
2025-08-01 09:56:58 +0200

    Update GitHub demo server

--- demo/server-github-demo/server.py
+++ demo/server-github-demo/server.py
@@ -1,7 +1,4 @@
-from datetime import datetime, timedelta, timezone
-
 import httpx
-from dateutil import parser
 from pydantic import BaseModel, Field
 
 from mcp.server.fastmcp import TMCP
@@ -12,47 +9,70 @@ mcp = TMCP("GitHubDemo", port=8001)
 
 
 @mcp.tool()
-async def get_recent_commits_somehow():
-    """Get the commit messages from the past week on the main branch of a GitHub repository for which we do not yet
-    know the name"""
+async def get_recent_commits(repository: str | None, owner: str | None):
+    """
+Get the commit messages of the 10 most recent commits on the main branch of a GitHub repository of a certain name \
+by a certain owner, starting with the most recent commit.
+You can call this tool without an explicit owner if they are not specified by the user. In that case, you enter null \
+(or None in Python terminology) for the owner, and we will ask the user who is the owner. You do not have to ask the \
+user to clarify the owner, as we will take care of that. So don't bother asking the user to specify the owner, \
+just call this tool with null for the owner if the owner is not clearly indicated by the user. \
+Also, please don't try to guess the owner, because then you will likely end up on the wrong repository or a \
+repository that doesn't exist.
+The same applies to the repository, which may also be null.\
+"""
 
-    class Repository(BaseModel):
-        owner: str = Field(description="Repository owner")
-        repository: str = Field(description="GitHub repository name")
+    if repository is None:
 
-    response = await mcp.get_context().elicit(
-        message="What repository do you want to use?",
-        schema=Repository,
-    )
+        class Repository(BaseModel):
+            repository: str = Field(description="GitHub repository")
 
-    if response.action != "accept":
-        return TextContent(type="text", text="The user declined to specify which repository to use.")
+        response = await mcp.get_context().elicit(
+            message="Which repository do you want to get the commits of?",
+            schema=Repository,
+        )
 
-    return get_recent_commits(response.data.owner, response.data.repository)
+        if response.action != "accept":
+            return TextContent(type="text", text="The user declined to specify which repository to use.")
 
+        repository = response.data.repository
 
-@mcp.tool()
-def get_recent_commits(owner: str, repository: str):
-    """Get the commit messages from the past week on the main branch of a GitHub repository of a certain name by a
-    certain owner, starting with the most recent commit"""
+    if owner is None:
+
+        class Owner(BaseModel):
+            owner: str = Field(description="Repository owner")
+
+        response = await mcp.get_context().elicit(
+            message=f"Who is the owner of the {repository} repository?",
+            schema=Owner,
+        )
+
+        if response.action != "accept":
+            return TextContent(type="text", text="The user declined to specify who is the owner of the repository.")
+
+        owner = response.data.owner
 
     response = httpx.get(f"https://api.github.com/repos/{owner}/{repository}/commits")
     if response.is_error:
-        return f"Could not get recent commits (reason: {response.reason_phrase})"
+        return (
+            f"Could not get recent commits from the {repository} repository owned by {owner} "
+            f"(reason: {response.reason_phrase})"
+        )
 
     data = response.json()
 
-    now = datetime.now(timezone.utc)
-
-    return [
-        {
-            "message": c["commit"]["message"],
-            "author": c["commit"]["author"]["name"],
-            "date": c["commit"]["author"]["date"],
-        }
-        for c in data
-        if now - parser.parse(c["commit"]["author"]["date"]) <= timedelta(weeks=1)
-    ]
+    return {
+        "repository": repository,
+        "owner": owner,
+        "commits": [
+            {
+                "message": c["commit"]["message"],
+                "author": c["commit"]["author"]["name"],
+                "date": c["commit"]["author"]["date"],
+            }
+            for c in data
+        ][:10],
+    }
 
 
 if __name__ == "__main__":
